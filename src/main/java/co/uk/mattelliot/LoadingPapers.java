b6483ed6facf15b72256b54c37f2443d7cf372f4
bluishmatt <bluishmatt@gmail.com>
2021-08-07 19:08:50 -0500

    corrected paper folder structure

--- src/main/java/co/uk/mattelliot/LoadingPapers.java
+++ src/main/java/co/uk/mattelliot/LoadingPapers.java
@@ -1,23 +1,32 @@
 package co.uk.mattelliot;
 
-import javafx.scene.control.Alert;
+
+import org.apache.commons.lang3.StringUtils;
+import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch;
+
+import java.util.LinkedList;
 
 public class LoadingPapers {
-    public Alert loadingAlert = new Alert(Alert.AlertType.INFORMATION);
-    public int progress = 0;
+    public static void main(String[] args) {
+        String s1 = "Computer_science_paper_2__HL.pdf";
+        String s2 = "Computer_science_paper_2__HL_markscheme.pdf";
+        String s3 = "9700_s04_ms.pdf";
+        String s4 = "9700_s04_qp_1.pdf";
 
-    public LoadingPapers(){
-            loadingAlert.setTitle("Alert!");
-            loadingAlert.setHeaderText("Converting past papers into PNG files.");
-            loadingAlert.setContentText(progress +"% complete.");
-            loadingAlert.show();
-    }
+        String s5 = "9700_w04_ms_6.pdf";
+        String s6 = "9700_w04_qp_6.pdf";
 
-    public int getProgress() {
-        return progress;
-    }
+        System.out.println(StringUtils.difference(s1, s2));
+        System.out.println(StringUtils.difference(s3, s4));
+        System.out.println(StringUtils.difference(s5, s6));
 
-    public void setProgress(int progress) {
-        this.progress += progress;
+
+        DiffMatchPatch dmp = new DiffMatchPatch();
+        LinkedList<DiffMatchPatch.Diff> diff = dmp.diffMain(s5, s6, true);
+        System.out.println(diff.get(1).getClass());
+
+
+
+
     }
 }
